71c3c5e32b0fbca38ffc16552eadab4f39a876d3
vturi3 <v.turi3@studenti.unisa.it>
2023-05-08 05:04:02 +0200

    final

--- data_generation.py
+++ data_generation.py
@@ -1,6 +1,6 @@
 import random
 import csv
-
+import numpy as np
 # Dati di produzione cumulativa mensili in kWh per un impianto di 5kW posto a Salerno
 prod_mensili = [235, 293, 466, 595, 772, 864, 933, 811, 588, 435, 262, 208]
 
@@ -10,39 +10,48 @@ giorni_mensili = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 # Calcolo della produzione giornaliera in kWh, in questo modo abbiamo la produzione totale media di un giorno per ogni mese dell'anno
 prod_giornaliera_media = [prod_mensili[i]/giorni_mensili[i] for i in range(len(prod_mensili))]
 
-# Approssimiamo alla seconda cifra decimale e calcoliamo i watt per ora
+# Approssimiamo alla seconda cifra decimale e calcoliamo i watt totali
 prod_giornaliera_media_rounded = [int(elem*1000) for elem in prod_giornaliera_media]
 print("prod giornaliera", prod_giornaliera_media_rounded)
 
-# Aggiunta della variazione giornaliera
+"""# Aggiunta della variazione giornaliera
 prod_giornaliera_variata = []
 for i in range(365):
-    prod_giornaliera_variata.append(round(prod_giornaliera_media_rounded[i%12] * (1 + random.uniform(-0.1, 0.1)), 2))
+    prod_giornaliera_variata.append(round(prod_giornaliera_media_rounded[i%12] * (1 + random.uniform(-0.1, 0.1)), 2))"""
 
 #print("VARIATA: ", prod_giornaliera_variata)
 
-def calcolo_prod_oraria(tot_produzione_giornaliera):
-    # Definiamo l'irradianza media oraria durante la giornata
-    # con valori in Watt/m^2
-    irradianza = [0, 0, 0, 80, 140, 200, 230, 250, 230, 200, 140, 60, 0]
+def calcolo_prod_oraria(tot_produzione_giornaliera,mese):
+    # estiva = [0, 0, 0, 0, 0, 0.01, 0.02, 0.03, 0.06, 0.08, 0.09, 0.11, 0.11, 0.11, 0.10, 0.09, 0.07, 0.05, 0.04, 0.02, 0.01, 0, 0, 0]
+    # invernale = [0, 0, 0, 0, 0, 0, 0, 0.02, 0.05, 0.09, 0.12, 0.13, 0.15, 0.13, 0.12, 0.09, 0.06, 0.03, 0.01, 0, 0, 0, 0, 0]
+    # autunnale = [0, 0, 0, 0, 0, 0.01, 0.02, 0.03, 0.05, 0.07, 0.08, 0.1, 0.11, 0.12, 0.11, 0.1, 0.09, 0.06, 0.03, 0.02, 0, 0, 0, 0]
+    # primaverile = [0, 0, 0, 0, 0, 0.01, 0.02, 0.03, 0.05, 0.07, 0.08, 0.1, 0.11, 0.12, 0.11, 0.1, 0.09, 0.06, 0.03, 0.02, 0, 0, 0, 0]
+    
+    if mese >=3 and mese <=5:
+        # primavera
+        distribuzione = [0, 0, 0, 0, 0, 0.01, 0.02, 0.03, 0.05, 0.07, 0.08, 0.1, 0.11, 0.12, 0.11, 0.1, 0.09, 0.06, 0.03, 0.02, 0, 0, 0, 0]
+    elif mese <=2 or mese == 12:
+        # inverno
+        distribuzione = [0, 0, 0, 0, 0, 0, 0, 0.02, 0.05, 0.09, 0.12, 0.13, 0.15, 0.13, 0.12, 0.09, 0.06, 0.03, 0.01, 0, 0, 0, 0, 0]
+    elif mese >=6 and mese <=8:
+        # estate
+        distribuzione = [0, 0, 0, 0, 0, 0.01, 0.02, 0.03, 0.06, 0.08, 0.09, 0.11, 0.11, 0.11, 0.10, 0.09, 0.07, 0.05, 0.04, 0.02, 0.01, 0, 0, 0]
+    elif mese >=9 and mese <=11:
+        # autunno
+        distribuzione = [0, 0, 0, 0, 0, 0.01, 0.02, 0.03, 0.05, 0.07, 0.08, 0.1, 0.11, 0.12, 0.11, 0.1, 0.09, 0.06, 0.03, 0.02, 0, 0, 0, 0]
+    x = (np.ones(24) * tot_produzione_giornaliera)
+    output = x * distribuzione
+    return output
 
-    # Calcoliamo la somma delle irradiazioni orarie
-    irradianza_sommata = sum(irradianza)
+valori_orari = []
+for i in range(1,13):
+    valori_orari.insert(i-1,calcolo_prod_oraria(prod_giornaliera_media_rounded[i-1],i))
+for i in range (0,12):
+    print("mese",i)
+    print("produzione oraria", valori_orari[i])
 
-    # Calcoliamo il fattore di scala per la produzione totale giornaliera
-    factor = tot_produzione_giornaliera / irradianza_sommata
-
-    # Calcoliamo la produzione oraria
-    prod_oraria = [x * factor for x in irradianza]
-
-    return prod_oraria
-
-valori_orari = [int(elem) for elem in calcolo_prod_oraria(prod_giornaliera_media_rounded[0])]
-print("produzione oraria", valori_orari)
-print("sum", sum(valori_orari))
-
-def produzione_mensile(profilo_giornaliero, mese):
-    """Genera un array di produzione mensile leggermente variato rispetto al profilo base"""
+"""def produzione_mensile(profilo_giornaliero, mese):
+    #Genera un array di produzione mensile leggermente variato rispetto al profilo base
     profilo_mensile = profilo_giornaliero.copy()
     if mese == 1 or mese == 2 or mese == 12:  # inverno
         variance = random.uniform(-0.3, 0.3)
@@ -60,7 +69,7 @@ def produzione_mensile(profilo_giornaliero, mese):
 
 prod_mensile = [int(elem) for elem in produzione_mensile(valori_orari,1)]
 print("array variato", prod_mensile)
-
+"""
 """
 # Calcolo della produzione oraria
 prod_oraria = []
